f4a3a7a5b32c5a897340a41e1ecbbab1d0d2b728
karolsimon <karolsimon95@gmail.com>
2024-12-22 22:15:08 +0100

    job done :)

---
+++ timer.js
@@ -0,0 +1,17 @@
+function countingdown()
+{
+    let today = new Date();
+    
+    let hour = today.getHours();
+    if (hour<10) hour = "0"+hour;
+
+    let minute = today.getMinutes();
+    if (minute<10) minute = "0"+minute;
+
+    let second = today.getSeconds();
+    if (second<10) second = "0"+second;
+
+    document.getElementById("clock").innerHTML = hour+":"+minute+":"+second;
+
+    setTimeout("countingdown()",1000);
+}
